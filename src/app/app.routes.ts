8c482df1c6eef9d5d78aa694d65a53810931bc51
chethanmj0430 <chethanmj0430@gmail.com>
2025-07-15 00:44:12 +0530

    roting fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
 export const appRoutes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./features/home/home.routes').then(m => m.HOME_ROUTES),
+    component: AppComponent,
   },
   {
     path: 'dashbaords',
--- src/app/core/auth.service.ts
+++ src/app/core/auth.service.ts
@@ -3,21 +3,19 @@ import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
-  private validUser = { username: 'admin', password: '1234' };
+  private validUser = { username: 'admin', password: 'admin123' };
 
   private _isLoggedIn = new BehaviorSubject<boolean>(!!localStorage.getItem('token'));
 
-  // 2. Expose an Observable that components can subscribe to.
-  // Using .asObservable() prevents external components from directly modifying the state.
   public isLoggedIn$: Observable<boolean> = this._isLoggedIn.asObservable();
   
   login(username: string, password: string): boolean {
     if (username === this.validUser.username && password === this.validUser.password) {
       localStorage.setItem('token', 'mock-jwt-token');
-      this._isLoggedIn.next(true); // Update the BehaviorSubject to true on successful login
+      this._isLoggedIn.next(true);
       return true;
     }
-    this._isLoggedIn.next(false); // Update the BehaviorSubject to false on failed login
+    this._isLoggedIn.next(false); 
     return false;
   }
 
